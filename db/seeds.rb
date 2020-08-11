ce8169ed7b159ec1f8abc07504d028812f56bfd3
Liam Cooke <noreply@liamcooke.com>
2020-08-11 15:04:01 +1000

    Create seed data for products and retailers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+bakery_retailer = Retailer.create(name: 'Bakery')
+dairy_retailer = Retailer.create(name: 'Dairy')
+supermarket_retailer = Retailer.create(name: 'Supermarket')
+
+Product.create([
+  {
+    name: 'Baguette',
+    price: '7.99'.to_d,
+    retailer: bakery_retailer,
+  },
+  {
+    name: 'Organic Sourdough',
+    price: '14.99'.to_d,
+    retailer: bakery_retailer,
+  },
+  {
+    name: 'Milk',
+    price: '4.99'.to_d,
+    retailer: dairy_retailer,
+  },
+  {
+    name: 'Avocado',
+    price: '4.99'.to_d,
+    retailer: supermarket_retailer,
+  },
+  {
+    name: 'Banana',
+    price: '1.50'.to_d,
+    retailer: supermarket_retailer,
+  },
+  {
+    name: 'Toilet Paper 6 Pack',
+    price: '5.99'.to_d,
+    retailer: supermarket_retailer,
+  },
+])
